01e27ef27d0f9dc1522434529d5b642bae021e47
andip71 <andreasp@gmx.de>
2016-07-28 19:14:50 +0200

    Boeffla ramfs: Kernel URL domain change

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -12,7 +12,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="oneplusx;cm;cm13.0;http://boeffla.df-kunde.de/oneplusx/boeffla-kernel-cm/"
+KERNEL_SPECS="oneplusx;cm;cm13.0;http://kernel.boeffla.de/oneplusx/boeffla-kernel-cm/"
 
 # kernel features 
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
