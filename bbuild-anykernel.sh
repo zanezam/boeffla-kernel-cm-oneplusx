4a7bfcaf7064d66e9a46e1067020905df0939be4
andip71 <andreasp@gmx.de>
2017-07-04 09:10:51 +0200

    bbuild: Add compiler flags required for disabled cc_optimize_for_size option

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -14,8 +14,8 @@ BOEFFLA_VERSION="1.0-test-CM-oneplusx"
 
 TOOLCHAIN="/opt/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 ARCHITECTURE=arm
-COMPILER_FLAGS_KERNEL=""
-COMPILER_FLAGS_MODULE=""
+COMPILER_FLAGS_KERNEL="-Wno-maybe-uninitialized -Wno-array-bounds"
+COMPILER_FLAGS_MODULE="-Wno-maybe-uninitialized -Wno-array-bounds"
 
 KERNEL_IMAGE="zImage-dtb"
 COMPILE_DTB="n"
